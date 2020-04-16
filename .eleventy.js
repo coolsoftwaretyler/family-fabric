f06beb34d2536e4a890025fddb7dbbaa7472af7c
Tyler Williams <tyler@ogdenstudios.xyz>
2020-04-16 17:51:02 -0600

    add deafult layout and img folder

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ const Terser = require("terser");
 
 module.exports = function (eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("src/fonts");
     eleventyConfig.addPassthroughCopy("src/img");
     
     eleventyConfig.addFilter("cssmin", function (code) {
